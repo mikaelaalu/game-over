c2f67564cdfe33722951e25cc8745b56fdd5a52a
mikaelaalu <54809602+mikaelaalu@users.noreply.github.com>
2020-05-22 14:23:37 +0200

    added background music

--- jump/index.js
+++ jump/index.js
@@ -19,11 +19,14 @@ let papersArray = [];
 let soapsArray = [];
 
 let gravity = 2;
+let backgroundMusic = new Audio("./sounds/smile.mp3");
+
 const startGameBtn = document.querySelector(".play-btn");
 const playContainer = document.querySelector(".play-game-container");
 const infoBox = document.querySelector(".info-box");
 
 startGameBtn.addEventListener("click", function () {
+  backgroundMusic.play();
   playContainer.classList.add("display-none");
   infoBox.classList.add(".display-block");
 });
